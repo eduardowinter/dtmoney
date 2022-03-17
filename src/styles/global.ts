56d3372ed0a659652661fa5b43bd8b62ef663c81
Eduardo Winter <eduardo.wbess@outlook.commit>
2022-03-17 09:25:33 -0300

    added modal and hook

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,8 +2,8 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: #f0f2f5;
     --red: #E52E4D;
+    --green: #33CC95; 
     --blue: #5429CC;
 
     --blue-light: #6933FF;
@@ -11,7 +11,7 @@ export const GlobalStyle = createGlobalStyle`
     --text-title: #363F5F;
     --text-body: #969CB3;
 
-    --background: : #F0F2F5;
+    --background: #F0F2F5;
     --shape: #FFFFFF;
   }
 
@@ -52,5 +52,42 @@ export const GlobalStyle = createGlobalStyle`
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
+  }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-model-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.24rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
   }
 `
